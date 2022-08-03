e1d2c146604119c71f3737d0a5d3158ad4b4df1a
genichiro22 <scepterjw@yahoo.co.jp>
2022-08-03 22:47:37 +0900

    status_codeと404例外処理を実装

--- blokus-api/main.py
+++ blokus-api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from cmath import pi
+from fastapi import FastAPI, Depends, status, Response
 from piece import rotate
 import json
 pieces_fr = rotate.pieces_fr
@@ -10,10 +11,19 @@ app = FastAPI()
 def index():
     return json.dumps(p_fr_base)
 
-@app.get("/{piece_name}/")
-def fr(piece_name):
+@app.get("/{piece_name}/", status_code=status.HTTP_200_OK)
+def fr(piece_name:str, response:Response):
+    if piece_name not in p_fr_base.keys():
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": f"Piece name of {piece_name} is not available"}
     return json.dumps(p_fr_base[piece_name])
 
-@app.get("/{piece_name}/{id}")
-def fr(piece_name:str,id:int):
+@app.get("/{piece_name}/{id}", status_code=status.HTTP_200_OK)
+def fr(piece_name:str, id:int, response:Response):
+    if piece_name not in p_fr_base.keys():
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": f"Piece name of {piece_name} is not available"}
+    if id>=len(p_fr_base[piece_name]):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": f"Index {id} is not found in flip-rotation of the piece {piece_name}"}
     return json.dumps(p_fr_base[piece_name][id])
