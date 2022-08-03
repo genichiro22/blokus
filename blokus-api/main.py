5a035be28ed535e344108049539dfb0071018660
genichiro22 <scepterjw@yahoo.co.jp>
2022-08-03 23:00:24 +0900

    return式からraise HTTPException式に

--- blokus-api/main.py
+++ blokus-api/main.py
@@ -1,5 +1,4 @@
-from cmath import pi
-from fastapi import FastAPI, Depends, status, Response
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from piece import rotate
 import json
 pieces_fr = rotate.pieces_fr
@@ -14,16 +13,22 @@ def index():
 @app.get("/{piece_name}/", status_code=status.HTTP_200_OK)
 def fr(piece_name:str, response:Response):
     if piece_name not in p_fr_base.keys():
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail": f"Piece name of {piece_name} is not available"}
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Piece name of {piece_name} is not available"
+        )
     return json.dumps(p_fr_base[piece_name])
 
 @app.get("/{piece_name}/{id}", status_code=status.HTTP_200_OK)
 def fr(piece_name:str, id:int, response:Response):
     if piece_name not in p_fr_base.keys():
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail": f"Piece name of {piece_name} is not available"}
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Piece name of {piece_name} is not available"
+        )
     if id>=len(p_fr_base[piece_name]):
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail": f"Index {id} is not found in flip-rotation of the piece {piece_name}"}
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Index {id} is not found in flip-rotation of the piece {piece_name}"
+        )
     return json.dumps(p_fr_base[piece_name][id])
