e62905e52585033fd166506178e1d309f4eb1e30
genichiro22 <scepterjw@yahoo.co.jp>
2022-08-22 18:25:41 +0900

    Authorizationに成功。せっかくなのでJinjaテンプレートにもプレイヤー名を表示。しかしplayerだったりuserだったりしてよくないので適当に名前リファクタリングが必要か

--- blokus-api/functions/frontend.py
+++ blokus-api/functions/frontend.py
@@ -1,8 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
 from fastapi.responses import HTMLResponse
 
-def get():
+def get(current_player):
     env = Environment(loader=FileSystemLoader('./templates/', encoding='utf8'))
     tmpl = env.get_template('render.j2')
-    c = tmpl.render()
+    c = tmpl.render(player_name=current_player.name)
     return HTMLResponse(content=c)
--- blokus-api/routes/frontend.py
+++ blokus-api/routes/frontend.py
@@ -5,11 +5,13 @@ from sqlalchemy.orm import Session
 from jinja2 import Template, Environment, FileSystemLoader
 from fastapi.responses import HTMLResponse
 from functions import frontend
+import oauth2
+import models
 
 router = APIRouter(
     tags = ["frontend"]
 )
 
 @router.get("/")
-def get_rendered_field():
-    return frontend.get()
+def get_rendered_field(current_player: models.Player=Depends(oauth2.get_current_user)):
+    return frontend.get(current_player)
